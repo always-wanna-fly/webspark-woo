4bd06fa235d9316c9530a407576d3901dcc35646
Володимир Іваніцький <volodimirivanitskyi@MacBook-Pro-Volodimir.local>
2025-02-10 12:46:08 +0200

    checking for the installation of woocommerce

--- wp-my-product-webspark.php
+++ wp-my-product-webspark.php
@@ -6,3 +6,30 @@
  * Text Domain: wp-my-product-webspark
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Check if WooCommerce is activated.
+ */
+function wp_my_product_webspark_check_woocommerce() {
+	if ( ! class_exists( 'WooCommerces' ) ) {
+		add_action( 'admin_notices', function () {
+			echo '<div class="error"><p>WooCommerce is required for this plugin to work</p></div>';
+		} );
+
+		return false;
+	}
+
+	return true;
+}
+
+/**
+ * Launching the plugin.
+ */
+function wp_my_product_webspark_init() {
+	if ( ! wp_my_product_webspark_check_woocommerce() ) {
+		return;
+	}
+}
